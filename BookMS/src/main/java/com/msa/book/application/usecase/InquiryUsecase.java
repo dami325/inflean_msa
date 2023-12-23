cc988399fe61956fab438f6205639fd6d56ce065
Judalm Park (dami) <wnekfa1004@naver.com>
2023-12-23 20:52:30 +0900

    응용서비스 구현(어플리케이션 헥사곤)

---
+++ BookMS/src/main/java/com/msa/book/application/usecase/InquiryUsecase.java
@@ -0,0 +1,8 @@
+package com.msa.book.application.usecase;
+
+import com.msa.book.framework.web.dto.BookOutPutDTO;
+
+public interface InquiryUsecase {
+
+    public BookOutPutDTO getBookInfo(long bookNo);
+}
---
+++ BookMS/src/main/java/com/msa/book/application/usecase/MakeAvailableUsecase.java
@@ -0,0 +1,9 @@
+package com.msa.book.application.usecase;
+
+import com.msa.book.framework.web.dto.BookOutPutDTO;
+
+public interface MakeAvailableUsecase {
+
+    public BookOutPutDTO available(Long bookNo);
+
+}
