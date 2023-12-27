013101996551a80af1f621d4c7bba897d44527f3
Judalm Park (dami) <wnekfa1004@naver.com>
2023-12-27 21:41:47 +0900

    외부여역 구현(프레임워크 헥사곤),API 테스트

--- MemberMS/src/main/java/com/msa/member/domain/model/Member.java
+++ MemberMS/src/main/java/com/msa/member/domain/model/Member.java
@@ -5,21 +5,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long memberNo;
+
+    @Embedded
     private IDName idName;
+
+    @Embedded
     private PassWord password;
+
+    @Embedded
     private Email email;
 
+    @ElementCollection
     private List<Authority> authorities = new ArrayList<Authority>();
 
+    @Embedded
     private Point point;
 
     public static Member registerMember(IDName idName, PassWord password, Email email) {
---
+++ MemberMS/src/main/java/com/msa/member/framework/jpaadapter/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.msa.member.framework.jpaadapter;
+
+import com.msa.member.domain.model.Member;
+import com.msa.member.domain.model.vo.IDName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    public Optional<Member> findMemberByIdName(IDName idName);
+
+}
